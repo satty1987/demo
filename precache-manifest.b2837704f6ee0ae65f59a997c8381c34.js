06cdc2459443de53075faa9ea06288baf17769a5
Ovi <muhammadowaisnaseem@gmail.com>
2019-10-28 01:19:08 +0500

    Updates

---
+++ precache-manifest.b2837704f6ee0ae65f59a997c8381c34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49c19c35be382798402e0df6aa701f6",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "92e6aa5a4b06297a6f00",
+    "url": "/demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a7ac75ac120fe6f4f5db",
+    "url": "/demo/static/js/2.c6e295a6.chunk.js"
+  },
+  {
+    "revision": "92e6aa5a4b06297a6f00",
+    "url": "/demo/static/js/main.37ea9d0e.chunk.js"
+  },
+  {
+    "revision": "be6901ea727d172032ec",
+    "url": "/demo/static/js/runtime-main.88913b19.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/demo/static/media/logo.25bf045c.svg"
+  }
+]);
